f1a7525d941400c21d5eab15f58c3fff783c1918
jlurz24 <jlurz24@gmail.com>
2015-10-12 20:38:35 -0400

    Add fall detection

---
+++ src/announced_fall_detector.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <human_catching/HumanFall.h>
+
+namespace {
+using namespace std;
+using namespace geometry_msgs;
+
+// TODO: Calibrate this height
+static const double DEFAULT_HEIGHT = 0.5;
+
+class AnnouncedFallDetector {
+private:
+    //! Publisher for the fall information
+	ros::Publisher fallPub;
+    
+	//! Node handle
+	ros::NodeHandle nh;
+
+	//! Private nh
+	ros::NodeHandle pnh;
+public:
+	AnnouncedFallDetector() :
+		pnh("~") {
+         fallPub = nh.advertise<human_catching::HumanFall>(
+				"out", 1);
+         geometry_msgs::Point torsoLocation;
+         pnh.param("x", torsoLocation.x, 0.0);
+         pnh.param("y", torsoLocation.y, 0.0);
+         pnh.param("z", torsoLocation.z, DEFAULT_HEIGHT);
+         publishFall(torsoLocation);
+	}
+    
+private:
+    void publishFall(const geometry_msgs::Point& torsoLocation){
+        human_catching::HumanFall fall;
+        fall.position = torsoLocation;
+        fall.header.frame_id = "/map";
+        fall.header.stamp = ros::Time::now();
+
+        // Publish the event
+        ROS_DEBUG("Publishing a human fall event");
+        fallPub.publish(fall);
+      }
+};
+}
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "announced_fall_detector");
+
+	AnnouncedFallDetector ahf;
+	ros::spin();
+}
---
+++ src/simulated_fall_detector.cpp
@@ -0,0 +1,59 @@
+#include <ros/ros.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <human_catching/HumanFall.h>
+
+namespace {
+using namespace std;
+using namespace geometry_msgs;
+
+class SimulatedFallDetector {
+private:
+    //! Publisher for the fall information
+	ros::Publisher fallPub;
+    
+	//! Node handle
+	ros::NodeHandle nh;
+
+	//! Private nh
+	ros::NodeHandle pnh;
+    
+    //! Cached service client.
+    ros::ServiceClient modelStateServ;
+public:
+	SimulatedFallDetector() :
+		pnh("~") {
+         fallPub = nh.advertise<human_catching::HumanFall>(
+				"out", 1);
+
+        ros::service::waitForService("/gazebo/get_model_state");
+        modelStateServ = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state", true /* persistent */);
+        publishFall(getHumanPosition());
+	}
+
+private:
+    geometry_msgs::Point getHumanPosition(){
+        gazebo_msgs::GetModelState modelState;
+        modelState.request.model_name = "human";
+        modelStateServ.call(modelState);
+        return modelState.response.pose.position;
+    }
+    
+    void publishFall(const geometry_msgs::Point& torsoLocation){
+        human_catching::HumanFall fall;
+        fall.position = torsoLocation;
+        fall.header.frame_id = "/map";
+        fall.header.stamp = ros::Time::now();
+
+        // Publish the event
+        ROS_DEBUG("Publishing a human fall event");
+        fallPub.publish(fall);
+      }
+};
+}
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "simulated_fall_detector");
+
+	SimulatedFallDetector sfd;
+	ros::spin();
+}
